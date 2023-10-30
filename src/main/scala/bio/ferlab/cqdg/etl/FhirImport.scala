3eca8f4e5d7dcd57f089f4fd646e9325d143f1c5
Adrian <adipaul1981@yahoo.ca>
2023-10-30 17:39:10 -0400

    fix: CQDG-00 fix key fetch

--- src/main/scala/bio/ferlab/cqdg/etl/FhirImport.scala
+++ src/main/scala/bio/ferlab/cqdg/etl/FhirImport.scala
@@ -39,7 +39,6 @@ object FhirImport extends App {
         val filesBucket = conf.aws.filesBucket
 
         val metadataInputPrefixMap = getMatadataPerRuns(s"$project/$study", filesBucket)
-        println(metadataInputPrefixMap.keySet.size)
 
         val auth: Auth = new AuthTokenInterceptor(conf.keycloak).auth
 
